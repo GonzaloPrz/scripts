7d3300fb26d082353b15bc7cb1319d028e41420f
gonzaloprz <gp@MacBook-Air-de-Gonzalo.local>
2024-10-28 13:53:13 -0300

    .

--- shared/report_best_classifier.py
+++ shared/report_best_classifier.py
@@ -65,77 +65,80 @@ for feature_selection in feature_selection_list:
         dimensions = [folder.name for folder in Path(results_dir,task).iterdir() if folder.is_dir()]
         for dimension in dimensions:
             print(task,dimension)
-            path = Path(results_dir,task,dimension,scaler_name,kfold_folder,y_label,'hyp_opt' if hyp_opt else 'hyp_opt' if hyp_opt else 'no_hyp_opt','feature_selection' if feature_selection else '','shuffle' if shuffle_labels else '')
-            
+            path = Path(results_dir,task,dimension,scaler_name,kfold_folder)
+
             if not path.exists():
                 continue
+            y_labels = [folder.name for folder in path.iterdir() if y_label.is_dir()]
+            for y_label in y_labels:
+                path = Path(results_dir,task,dimension,scaler_name,kfold_folder,y_label,'hyp_opt' if hyp_opt else 'hyp_opt' if hyp_opt else 'no_hyp_opt','feature_selection' if feature_selection else '','shuffle' if shuffle_labels else '')
+            
+                random_seeds_test = [folder.name for folder in path.iterdir() if folder.is_dir() if 'random_seed' in folder.name]
 
-            random_seeds_test = [folder.name for folder in path.iterdir() if folder.is_dir() if 'random_seed' in folder.name]
+                if len(random_seeds_test) == 0:
+                    random_seeds_test = ['']
 
-            if len(random_seeds_test) == 0:
-                random_seeds_test = ['']
-
-            for random_seed_test in random_seeds_test:
-                
-                files = [file for file in Path(path,random_seed_test).iterdir() if f'all_models_norm_cross_entropy' in file.stem and 'test' in file.stem]
-                if len(files) == 0:
-                    files = [file for file in Path(path,random_seed_test).iterdir() if f'all_models_' in file.stem and 'dev' in file.stem]
-
-                best = None
-                for file in files:
+                for random_seed_test in random_seeds_test:
                     
-                    df = pd.read_csv(file)
-                    
-                    if f'{extremo}_{scoring}' in df.columns:
-                        scoring_col = f'{extremo}_{scoring}'
-                    else:
-                        scoring_col = f'{extremo}_{scoring}'
+                    files = [file for file in Path(path,random_seed_test).iterdir() if f'all_models_norm_cross_entropy' in file.stem and 'test' in file.stem]
+                    if len(files) == 0:
+                        files = [file for file in Path(path,random_seed_test).iterdir() if f'all_models_' in file.stem and 'dev' in file.stem]
 
-                    df = df.sort_values(by=scoring_col,ascending=ascending)
-                    
-                    print(f'{file.stem.split("_")[-2]}:{df.loc[0,scoring_col]}')
-                    if best is None:
-                        best = df.loc[0,:]
+                    best = None
+                    for file in files:
                         
-                        model_type = file.stem.split('_')[-2]
-                        best['model_type'] = model_type
-                        best['model_index'] = df.index[0]
-                        best_file = file
-                    else:
-                        if new_best(best[scoring_col],df.loc[0,scoring_col],ascending):
+                        df = pd.read_csv(file)
+                        
+                        if f'{extremo}_{scoring}' in df.columns:
+                            scoring_col = f'{extremo}_{scoring}'
+                        else:
+                            scoring_col = f'{extremo}_{scoring}'
+
+                        df = df.sort_values(by=scoring_col,ascending=ascending)
+                        
+                        print(f'{file.stem.split("_")[-2]}:{df.loc[0,scoring_col]}')
+                        if best is None:
                             best = df.loc[0,:]
+                            
                             model_type = file.stem.split('_')[-2]
                             best['model_type'] = model_type
                             best['model_index'] = df.index[0]
                             best_file = file
-                if best is None:
-                    continue
-                
-                print(best['model_type'])
-                for metric in metrics_names:
-                    try:
-                        best[f'{metric}_dev'] = f'[{np.round(best[f"inf_{metric}_dev"],2)}, {np.round(best[f"mean_{metric}_dev"],2)}, {np.round(best[f"sup_{metric}_dev"],2)}]'
-                    except:
-                        best[f'{metric}_dev'] = f'[{np.round(best[f"inf_{metric}"],2)}, {np.round(best[f"mean_{metric}"],2)}, {np.round(best[f"sup_{metric}"],2)}]'
-
-                    best[f'{metric}_holdout'] = np.nan
-                    try:
-                        mean = np.round(best[f'mean_{metric}_test'],2)
-                        inf = np.round(best[f'inf_{metric}_test'],2)
-                        sup = np.round(best[f'sup_{metric}_test'],2)
-                        best[f'{metric}_holdout'] = f'[ {inf}, {mean}, {sup}]'
-                    except:
+                        else:
+                            if new_best(best[scoring_col],df.loc[0,scoring_col],ascending):
+                                best = df.loc[0,:]
+                                model_type = file.stem.split('_')[-2]
+                                best['model_type'] = model_type
+                                best['model_index'] = df.index[0]
+                                best_file = file
+                    if best is None:
                         continue
+                    
+                    print(best['model_type'])
+                    for metric in metrics_names:
+                        try:
+                            best[f'{metric}_dev'] = f'[{np.round(best[f"inf_{metric}_dev"],2)}, {np.round(best[f"mean_{metric}_dev"],2)}, {np.round(best[f"sup_{metric}_dev"],2)}]'
+                        except:
+                            best[f'{metric}_dev'] = f'[{np.round(best[f"inf_{metric}"],2)}, {np.round(best[f"mean_{metric}"],2)}, {np.round(best[f"sup_{metric}"],2)}]'
 
-                model_type = file
-                
-                dict_append = {'task':task,'dimension':dimension,'model_type':best['model_type'],'model_index':best['model_index'],'random_seed_test':random_seed_test}
-                dict_append.update(dict((f'{metric}_dev',best[f'{metric}_dev']) for metric in metrics_names))
-                dict_append.update(dict((f'{metric}_holdout',best[f'{metric}_holdout']) for metric in metrics_names))
+                        best[f'{metric}_holdout'] = np.nan
+                        try:
+                            mean = np.round(best[f'mean_{metric}_test'],2)
+                            inf = np.round(best[f'inf_{metric}_test'],2)
+                            sup = np.round(best[f'sup_{metric}_test'],2)
+                            best[f'{metric}_holdout'] = f'[ {inf}, {mean}, {sup}]'
+                        except:
+                            continue
 
-                best_classifiers.loc[len(best_classifiers),:] = pd.Series(dict_append)
+                    model_type = file
+                    
+                    dict_append = {'task':task,'dimension':dimension,'model_type':best['model_type'],'model_index':best['model_index'],'random_seed_test':random_seed_test}
+                    dict_append.update(dict((f'{metric}_dev',best[f'{metric}_dev']) for metric in metrics_names))
+                    dict_append.update(dict((f'{metric}_holdout',best[f'{metric}_holdout']) for metric in metrics_names))
 
-        filename_to_save = f'best_classifiers_{scoring}_{kfold_folder}_{scaler_name}_no_hyp_opt_feature_selection_shuffled.csv'
+                    best_classifiers.loc[len(best_classifiers),:] = pd.Series(dict_append)
+
+            filename_to_save = f'best_classifiers_{scoring}_{kfold_folder}_{scaler_name}_no_hyp_opt_feature_selection_shuffled.csv'
 
     if hyp_opt:
         filename_to_save = filename_to_save.replace('no_hyp_opt','hyp_opt')
--- shared/report_best_regressor.py
+++ shared/report_best_regressor.py
@@ -23,6 +23,9 @@ def new_best(current_best,value,ascending):
     
 project_name = 'GeroApathy'
 scaler_name = 'StandardScaler'
+shuffle_labels = False
+
+metrics_names = ['r2_score','mean_absolute_error']
 
 tasks = ['Fugu']
 
@@ -42,10 +45,11 @@ l2ocv = False
 if l2ocv:
     kfold_folder = 'l2ocv'
 else:
-    n_folds = 10
+    n_folds = 5
     kfold_folder = f'{n_folds}_folds'
 
 hyp_opt = True
+mean_std = False
 
 feature_selection_list = [True]
 
@@ -63,32 +67,37 @@ models_dict = {'ridge':RR,
 
 results_dir = Path(Path.home(),'results',project_name) if 'Users/gp' in str(Path.home()) else Path('D:','results',project_name)
 for feature_selection in feature_selection_list:
-    for task in tasks[project_name]:
+    for task in tasks:
         dimensions = [folder.name for folder in Path(results_dir,task).iterdir() if folder.is_dir()]
         for dimension in dimensions:
             print(task,dimension)
-            path = Path(results_dir,task,dimension,scaler_name,kfold_folder,y_label,'hyp_opt' if hyp_opt else 'hyp_opt' if hyp_opt else 'no_hyp_opt','feature_selection' if feature_selection else '','shuffle' if shuffle_labels else '')
-            
+            path = Path(results_dir,task,dimension,scaler_name,kfold_folder,'mean_std' if mean_std else '')
+
             if not path.exists():
                 continue
+            y_labels = [folder.name for folder in path.iterdir() if folder.is_dir() if 'mean_std' not in folder.name]
+            
+            for y_label in y_labels:
+                path = Path(results_dir,task,dimension,scaler_name,kfold_folder,y_label,'hyp_opt' if hyp_opt else 'hyp_opt' if hyp_opt else 'no_hyp_opt','feature_selection' if feature_selection else '','shuffle' if shuffle_labels else '')
+            
+                random_seeds_test = [folder.name for folder in path.iterdir() if folder.is_dir() if 'random_seed' in folder.name]
 
-            random_seeds_test = [folder.name for folder in path.iterdir() if folder.is_dir() if 'random_seed' in folder.name]
-
-            if len(random_seeds_test) == 0:
-                random_seeds_test = ['']
-            for random_seed_test in random_seeds_test:
-                    files = [file for file in Path(path_to_data,f'random_seed_{random_seed_test}').iterdir() if 'all_models_r2_score' in file.stem and 'test' in file.stem]
+                if len(random_seeds_test) == 0:
+                    random_seeds_test = ['']
+                for random_seed_test in random_seeds_test:
+                    files = [file for file in Path(path,random_seed_test).iterdir() if 'all_models_r2_score' in file.stem and 'test' in file.stem]
                     
                     if len(files) == 0:
                         files = [file for file in Path(path,random_seed_test).iterdir() if f'all_models_' in file.stem and 'dev' in file.stem]
 
+                    best = None
                     for file in files:
                         df = pd.read_csv(file)
                         
                         if f'{extremo}_{scoring}' in df.columns:
                             scoring_col = f'{extremo}_{scoring}'
                         else:
-                            scoring_col = f'{extremo}_{scoring}'
+                            scoring_col = f'{extremo}_{scoring}_dev'
 
                         df = df.sort_values(by=scoring_col,ascending=ascending)
                         
@@ -108,33 +117,30 @@ for feature_selection in feature_selection_list:
                                 best['model_index'] = df.index[0]
                                 best_file = file
                     if best is None:
-                        continue
-        
+                            continue
+            
                     print(best['model_type'])
-                    r2_score_bootstrap = f'[{np.round(best["inf_r2_score_bootstrap"],2)}, {np.round(best["mean_r2_score_bootstrap"],2)}, {np.round(best["sup_r2_score_bootstrap"],2)}]'
-                    mean_squared_error_bootstrap = f'[{np.round(np.sqrt(best[f'inf_mean_squared_error_bootstrap']),2)}, {np.round(np.sqrt(best[f"mean_mean_squared_error_bootstrap"]),2)}, {np.round(np.sqrt(best[f"sup_mean_squared_error_bootstrap"]),2)}]'
-                    mean_absolute_error_bootstrap = f'[{np.round(best["inf_mean_absolute_error_bootstrap"],2)}, {np.round(best["mean_mean_absolute_error_bootstrap"],2)}, {np.round(best["sup_mean_absolute_error_bootstrap"],2)}]'
+                    for metric in metrics_names:
+                        try:
+                            best[f'{metric}_dev'] = f'[{np.round(best[f"inf_{metric}_dev"],2)}, {np.round(best[f"mean_{metric}_dev"],2)}, {np.round(best[f"sup_{metric}_dev"],2)}]'
+                        except:
+                            best[f'{metric}_dev'] = f'[{np.round(best[f"inf_{metric}"],2)}, {np.round(best[f"mean_{metric}"],2)}, {np.round(best[f"sup_{metric}"],2)}]'
 
-                    r2_score_bootstrap_test = 'NA'
-                    mean_squared_error_bootstrap_test = 'NA'
-                    mean_absolute_error_bootstrap_test = 'NA'
+                        best[f'{metric}_holdout'] = np.nan
+                        try:
+                            mean = np.round(best[f'mean_{metric}_test'],2)
+                            inf = np.round(best[f'inf_{metric}_test'],2)
+                            sup = np.round(best[f'sup_{metric}_test'],2)
+                            best[f'{metric}_holdout'] = f'[ {inf}, {mean}, {sup}]'
+                        except:
+                            continue
 
-                    if Path(best_file.parent,f'best_10_{best['model_type']}_test.csv').exists():
-                        
-                        best_test = pd.read_csv(Path(best_file.parent,f'best_10_{best['model_type']}_test.csv')).loc[0,:]
-                        
-                        r2_score_bootstrap_test = f'[{np.round(best_test["inf_r2_score_bootstrap_test"],2)}, {np.round(best_test["mean_r2_score_bootstrap_test"],2)}, {np.round(best_test["sup_r2_score_bootstrap_test"],2)}]'
-                        root_mean_squared_error_bootstrap_test = f'[{np.round(np.sqrt(best_test[f'inf_mean_squared_error_bootstrap_test']),2)}, {np.round(np.sqrt(best_test[f"mean_mean_squared_error_bootstrap_test"]),2)}, {np.round(np.sqrt(best_test[f"sup_mean_squared_error_bootstrap_test"]),2)}]'
-                        mean_absolute_error_bootstrap_test = f'[ {np.round(best_test["inf_mean_absolute_error_bootstrap_test"],2)}, {np.round(best_test["mean_mean_absolute_error_bootstrap_test"],2)}, {np.round(best_test["sup_mean_absolute_error_bootstrap_test"],2)}]'
+                    model_type = file
                     
-                    best_regressors.loc[len(best_regressors),:] = pd.Series({'task':task,'dimension':dimension,'target':y_label,'model_type':best['model_type'],
-                                                                                'r2_score_bootstrap_dev':r2_score_bootstrap,
-                                                                                'r2_score_bootstrap_holdout':r2_score_bootstrap_test,
-                                                                                'root_mean_squared_error_bootstrap_dev':mean_squared_error_bootstrap,
-                                                                                'root_mean_squared_error_bootstrap_holdout':root_mean_squared_error_bootstrap_test,
-                                                                                'mean_absolute_error_bootstrap_dev':mean_absolute_error_bootstrap,
-                                                                                'mean_absolute_error_bootstrap_holdout':mean_absolute_error_bootstrap_test
-                                                                                })
+                    dict_append = {'task':task,'dimension':dimension,'y_label':y_label,'model_type':best['model_type'],'model_index':best['model_index'],'random_seed_test':random_seed_test}
+                    dict_append.update(dict((f'{metric}_dev',best[f'{metric}_dev']) for metric in metrics_names))
+                    dict_append.update(dict((f'{metric}_holdout',best[f'{metric}_holdout']) for metric in metrics_names))
+
                 best_config = pd.read_csv(best_file).iloc[0,:].dropna()
                 best_params = [col for col in best_config.index if all([x not in col for x in task.split('__') + ['mean','inf','sup']])]
                 best_features = [col for col in best_config.index if any([x in col for x in task.split('__')]) and best_config[col] == 1]
@@ -146,12 +152,17 @@ for feature_selection in feature_selection_list:
                         params_dict['gamma'] = float(params_dict['gamma'])
                     except:
                         pass
-                    
-                #best_model_ = Model(models_dict[best['model_type']](**params_dict),StandardScaler)
 
-                #best_model_.model.feature_names_in_ = best_features 
+                best_regressors.loc[len(best_regressors),:] = pd.Series(dict_append)
 
-                #pickle.dump(best_model_,open(Path(best_file.parent,f'best_model_{best['model_type']}.pkl'),'wb'))
-                
-    filename_to_save = f'best_regressors_{kfold_folder}_hyp_opt.csv' if hyp_opt else f'best_regressors_{kfold_folder}_no_hyp_opt.csv'
+    filename_to_save = f'best_regressors_{scoring}_{kfold_folder}_{scaler_name}_no_hyp_opt_feature_selection_shuffled.csv'
+
+    if hyp_opt:
+        filename_to_save = filename_to_save.replace('no_hyp_opt','hyp_opt')
+    if not feature_selection:
+        filename_to_save = filename_to_save.replace('_feature_selection','')
+    if not shuffle_labels:
+        filename_to_save = filename_to_save.replace('_shuffled','')
+
+    best_regressors.dropna(axis=1,inplace=True)
     best_regressors.to_csv(Path(results_dir,filename_to_save),index=False)
