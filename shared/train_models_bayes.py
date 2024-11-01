ded8c29d654a1b1b6caac5def705bd5579332294
gonzaloprz <gp@MacBook-Air-de-Gonzalo.local>
2024-10-31 22:46:25 -0300

    .

--- shared/train_models_bayes.py
+++ shared/train_models_bayes.py
@@ -29,7 +29,7 @@ from expected_cost.ec import *
 from expected_cost.utils import *
 
 ##---------------------------------PARAMETERS---------------------------------##
-project_name = 'GeroApathy'
+project_name = 'MCI_classifier'
 
 l2ocv = False
 
