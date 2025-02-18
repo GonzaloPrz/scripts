42793e3799c6a15761293ce71c9dd3ceffdea3e9
Gonzalo <31898555+GonzaloPrz@users.noreply.github.com>
2025-02-18 18:20:39 -0300

    .

--- shared/merge_features_and_labels.py
+++ shared/merge_features_and_labels.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from pathlib import Path
 
-project_name = 'AKU'
-feature_filename = 'AKU_data_HC_all.csv'
-labels_filename = 'AKU_data_complete.csv'
+project_name = 'MPLS'
+feature_filename = 'features.csv'
+labels_filename = 'nps_data.csv'
 
 data_dir = Path(Path.home(),'data',project_name) if 'Users/gp' in str(Path.home()) else Path('D:','CNC_Audio','gonza','data',project_name)
 
@@ -13,4 +13,5 @@ labels = pd.read_excel(Path(data_dir,labels_filename)) if 'xlsx' in labels_filen
 
 all_data = pd.merge(features,labels[[col for col in labels.columns if '__' not in col]],on='id',how='outer')
 all_data.drop_duplicates(subset=['id'],inplace=True)
-all_data.to_csv(Path(data_dir,'all_data_HC.csv'),index=False)
+all_data.dropna(subset=['tell_id'],inplace=True)
+all_data.to_csv(Path(data_dir,'all_data.csv'),index=False)
