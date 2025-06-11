a4c3bf05a735d3ce9a8ece5d763c965759a943af
npelella <nicolaspelella@gmail.com>
2025-06-11 15:57:14 -0300

    Testeo de modelos con búsqueda bayesiana

--- shared/test_final_model_bayes.py
+++ shared/test_final_model_bayes.py
@@ -33,99 +33,6 @@ from scipy.stats import bootstrap
 sys.path.append(str(Path(Path.home(),'scripts_generales'))) if 'Users/gp' in str(Path.home()) else sys.path.append(str(Path(Path.home(),'gonza','scripts_generales')))
 
 import utils
-def _calculate_metrics(indices, outputs, y, metrics_names, prob_type, cost_matrix):
-    """
-    Statistic function for bootstrap. Calculates differences for ALL metrics at once.
-    """
-    # Resample y, ensuring we don't operate on an empty or invalid slice
-    resampled_y = y[indices].ravel()
-
-    # If a resample is degenerate (e.g., missing a class), metric calculation is impossible.
-    # Return NaNs to signal this. The 'bca' method will fail, triggering our fallback.
-    if np.unique(resampled_y).shape[0] != np.unique(y).shape[0]:
-        return np.full(len(metrics), np.nan)
-
-    # Resample model outputs
-    resampled_out = outputs[indices, :].reshape(-1, outputs.shape[-1])
-
-    # Get metrics for both classifiers
-    if prob_type == 'clf':
-        metrics, _ = utils.get_metrics_clf(resampled_out, resampled_y, metrics_names, cmatrix=cost_matrix)
-    else: # 'reg'
-        metrics = utils.get_metrics_reg(resampled_out, resampled_y, metrics_names)
-
-    # Return an array of differences
-    return np.array([metrics[m] for m in metrics_names])
-
-def test_models_bootstrap(model_class,params,features,scaler,imputer,calmethod,calparams,X_dev,y_dev,X_test,y_test,metrics_names,problem_type,threshold=None,cmatrix=None,priors=None,calibrate=False):
-    if not isinstance(X_dev,pd.DataFrame):
-        X_dev = pd.DataFrame(X_dev.squeeze(),columns=features)
-
-    if not isinstance(X_test,pd.DataFrame):
-        X_test = pd.DataFrame(X_test.squeeze(),columns=features)
-
-    if 'gamma' in params.keys():
-        try: 
-            params['gamma'] = float(params['gamma'])
-        except:
-            pass
-    if 'probability' in params.keys():
-        params['probability'] = True
-
-    outputs = utils.test_model(model_class,params,scaler,imputer, X_dev[features], y_dev, X_test[features], problem_type=problem_type)
-        
-    if calibrate:
-        model = utils.Model(model_class(**params),scaler,imputer,calmethod,calparams)
-        model.train(X_dev[features], y_dev)
-        outputs_dev = model.eval(X_dev[features],problem_type)
-    
-        outputs,_ = model.calibrate(outputs,None,outputs_dev,y_dev)
-         
-    n_samples = X_test.shape[0]
-    data_indices = (np.arange(n_samples),)
-
-    # Define the statistic function with data baked in
-    stat_func = lambda indices: _calculate_metrics(
-        indices, outputs, y_test, 
-        metrics_names, problem_type, cmatrix
-    )
-
-    # 1. Calculate the point estimate (the actual difference on the full dataset)
-    point_estimates = stat_func(data_indices[0])
-    result = {}
-
-    # 2. Calculate the bootstrap confidence interval
-    try:
-        # Try the more accurate BCa method first
-        res = bootstrap(
-            data_indices,
-            stat_func,
-            n_resamples=n_boot_test, # Use configured n_boot
-            method='bca',
-            vectorized=False,
-            random_state=42
-        )
-        result["bootstrap_method_holdout"] = 'bca'
-
-    except ValueError as e:
-        # If BCa fails (e.g., due to degenerate samples), fall back to percentile
-        print(f"WARNING: BCa method failed for {task}/{dimension}/{y_label}. Falling back to 'percentile'. Error: {e}")
-        res = bootstrap(
-            data_indices,
-            stat_func,
-            n_resamples=n_boot_test,
-            method='percentile',
-            vectorized=False,
-            random_state=42
-        )
-        result["bootstrap_method_holdout"] = 'percentile'                    
-                
-    for i, metric in enumerate(metrics_names):
-        est = point_estimates[i]
-        ci_low, ci_high = res.confidence_interval.low[i], res.confidence_interval.high[i]
-        result[f"{metric}_holdout"] = f"{est:.3f}, ({ci_low:.3f}, {ci_high:.3f})"
-
-    return result
 
 late_fusion = False
 
@@ -135,6 +42,7 @@ project_name = config["project_name"]
 scaler_name = config['scaler_name']
 stat_folder = config['stat_folder']
 kfold_folder = config['kfold_folder']
+hyp_opt = config["n_iter"] > 0 
 shuffle_labels = config['shuffle_labels']
 feature_selection = config['feature_selection']
 filter_outliers = config['filter_outliers']
@@ -211,18 +119,62 @@ for scoring in scoring_metrics:
 
         metrics_names = list(set(metrics_names_) - set(['roc_auc','f1','recall','precision'])) if cmatrix is not None or len(np.unique(y_train)) > 2 else metrics_names_
         
-        result_append = test_models_bootstrap(type(trained_model),params,features,type(trained_scaler),type(trained_imputer),None,None,X_train,y_train,X_test,y_test,metrics_names,problem_type,threshold=None,cmatrix=cmatrix)
-        
-        result_append.update({'task':task,'dimension':dimension,'y_label':y_label,'model_type':model_type,'random_seed_test':random_seed_test, "bootstrap_method_dev": row["bootstrap_method_dev"]})
+        outputs = trained_model.eval(X_test[features])
 
-        for metric in metrics_names:
-            try:
-                result_append.update({f'{metric}_dev':row[metric]})
-            except:
-                continue
-        if results_test.empty:
-            results_test = pd.DataFrame(result_append,index=[0])
-        else:
-            results_test = pd.concat([results_test,pd.DataFrame(result_append,index=[0])],ignore_index=True)
-    results_test.to_csv(Path(results_dir,f'best_models_{kfold_folder}_{scoring}_{stat_folder}_feature_selection_test.csv'.replace('__','_') if feature_selection else
-                             f'best_models_{kfold_folder}_{scoring}_{stat_folder}_test.csv'.replace('__','_')))
+        # Prepare data for bootstrap: a tuple of index arrays to resample
+        data_indices = (np.arange(y_test.shape[-1]),)
+
+        # Define the statistic function with data baked in
+        stat_func = lambda indices: utils._calculate_metrics(
+            indices, outputs, y_test, 
+            metrics_names, problem_type, cmatrix
+        )
+
+        # 1. Calculate the point estimate (the actual difference on the full dataset)
+        point_estimates = stat_func(data_indices[0])
+
+        # 2. Calculate the bootstrap confidence interval
+        try:
+            # Try the more accurate BCa method first
+            res = bootstrap(
+                data_indices,
+                stat_func,
+                n_resamples=n_boot_test, # Use configured n_boot
+                method=config["bootstrap_method"],
+                vectorized=False,
+                random_state=42
+            )
+            bootstrap_method = config["bootstrap_method"]
+
+        except ValueError as e:
+            # If BCa fails (e.g., due to degenerate samples), fall back to percentile
+            print(f"WARNING: {config['bootstrap_method']} method failed for {task}/{dimension}/{y_label}. Falling back to 'percentile'. Error: {e}")
+            res = bootstrap(
+                data_indices,
+                stat_func,
+                n_resamples=n_boot_test,
+                method='percentile',
+                vectorized=False,
+                random_state=42
+            )
+            bootstrap_method = 'percentile'
+
+            row.update({'bootstrap_method':bootstrap_method})
+            for i, metric in enumerate(metrics_names):
+                est = point_estimates[i]
+                ci_low, ci_high = res.confidence_interval.low[i], res.confidence_interval.high[i]
+                row.update({f'{metric}_holdout': f"{est:.3f}, ({ci_low:.3f}, {ci_high:.3f})"})
+
+    filename_to_save = f'best_models_{kfold_folder}_{scoring}_{stat_folder}_hyp_opt_feature_selection_shuffle_test.csv'
+
+    if not hyp_opt:
+        filename_to_save = filename_to_save.replace('_hyp_opt','')
+    
+    if not feature_selection:
+        filename_to_save = filename_to_save.replace('_feature_selection','')
+    
+    if not shuffle_labels:
+        filename_to_save = filename_to_save.replace('_shuffle','')
+        
+    best_models.to_csv(Path(results_dir,filename_to_save),ignore_axis=True)
+        
